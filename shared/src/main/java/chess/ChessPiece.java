3d4d183ee508a698b6de565e68d7e50e699a3f59
Paige Martin <mpaigek0@byu.edu>
2026-09-13 16:09:45 -0600

    finished 'getter' functions for teamcolor and type. Beginning to lay out the if else if block for different pieces. Going to next add arrays for all their possible move slots.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +11,27 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
 
+    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    // checks where pieces are
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -29,15 +50,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-
-
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +68,30 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new ArrayList<>(); // make an array list to fill with each piece's possible moves
+        PieceType type = getPieceType();
+        if (type == PieceType.BISHOP){
+            // bishop can move in the 4 diagonals from where he currently is.
+
+            return; // NEED TO IMPLEMENT
+        }
+        else if (type == PieceType.ROOK){
+
+        }
+
+        else if (type == PieceType.QUEEN){
+
+        }
+        else if (type == PieceType.KNIGHT){
+
+        }
+
+        else if (type == PieceType.KING){
+
+        }
+        else if (type == PieceType.PAWN){
+
+        }
+        return; // NEED TO IMPLEMENT
     }
 }
