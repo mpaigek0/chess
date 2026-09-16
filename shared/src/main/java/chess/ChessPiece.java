f62147d7ae972ced917ad5d1a95fc3eda7ec526f
Paige Martin <mpaigek0@byu.edu>
2026-09-16 12:28:19 -0600

    made jumping pieces helper function. need to fix the border cases for it now.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -68,7 +68,7 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
 
-    // check that the sliding moves for bishop, rook, & queen are valid
+    // helper function that checks that the sliding moves for bishop, rook, & queen are valid
     private void slideSquares(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int dRow, int dCol){
         int r = myPosition.getRow() + dRow; // gets current position and adds rol/col to see where trying to move
         int c = myPosition.getColumn() + dCol;
@@ -84,7 +84,7 @@ public class ChessPiece {
 
             }
             else{
-                // if there's a piece at the spot and it's not your piece, then stop sliding
+                // if there's a piece at the spot and it's not your color, then it can be overtaken but no more sliding
                 if (occupant.getTeamColor() != getTeamColor()){
                     moves.add(new ChessMove(myPosition, nextPos, null));
                 }
@@ -97,9 +97,29 @@ public class ChessPiece {
 
     }
 
+    // helper function for at least the knight and king
+    private void jumpSquares(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int dRow, int dCol){
+        int r = myPosition.getRow() + dRow; // gets current position and adds rol/col to see where trying to move
+        int c = myPosition.getColumn() + dCol;
+        ChessPosition nextPos = new ChessPosition(r, c);
+        ChessPiece occupant = board.getPiece(nextPos);
+        if (occupant == null){
+            moves.add(new ChessMove(myPosition, nextPos, null));
+
+        }
+        else{
+            // if there's a piece at the spot and it's not your color, then it can be overtaken but no more sliding
+            if (occupant.getTeamColor() != getTeamColor()){
+                moves.add(new ChessMove(myPosition, nextPos, null));
+            }
+        }
+        // check the next square in the same direction. Stop checking once a same color piece is there
+        r += dRow;
+        c += dCol;
+    }
 
 
-    // FUNCTION GIVEN TO US:
+    // FUNCTION OUTLINE GIVEN TO US:
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>(); // make an array list to fill with each piece's possible moves
         PieceType type = getPieceType();
@@ -138,8 +158,21 @@ public class ChessPiece {
 
         }
         else if (type == PieceType.KNIGHT){
-
-        }
+        // knight moves in an L-shape pattern: two squares in one direction (either vertically
+            // or horizontally, and then one square perpendicular to that direction
+            // it's the only piece that can jump over other pieces during its move
+            // every time a knight moves, it always lands on a square of the opposite color
+            // it captures a piece by landing on its square
+            // knight_directs = {1,2}, {2,1}, {1, -2}, {2, -1}, {-1, 2}, {-2, 1}, {-1,-2} {-2,-1}
+            jumpSquares(board, myPosition, moves, 1,2);
+            jumpSquares(board, myPosition, moves, 2,1);
+            jumpSquares(board, myPosition, moves, 1,-2);
+            jumpSquares(board, myPosition, moves, 2,-1);
+            jumpSquares(board, myPosition, moves, -1,2);
+            jumpSquares(board, myPosition, moves, -2,1);
+            jumpSquares(board, myPosition, moves, -1,-2);
+            jumpSquares(board, myPosition, moves, -2,-1);
+            }
 
         else if (type == PieceType.KING){
 
